a37fd0ff24b006c0be8e021cb50f5a9075e03655
Arjun Menon Vadakkeveedu <arjunmenonv@arjun-asus-20.04>
2022-05-15 10:10:06 +0530

    feat: add traces + data gen py code

--- SpMV_arm/helper_py/gen_data.py
+++ SpMV_arm/helper_py/gen_data.py
@@ -5,6 +5,7 @@
 '''
 
 import numpy as np 
+import random
 from scipy.sparse import csr_matrix
 
 def irregSparse():
@@ -17,14 +18,29 @@ def irregSparse():
     inpVectors = np.random.randn(numInputs, matrixWidth).reshape((numInputs, matrixWidth)) + np.ones((numInputs, matrixWidth))
     return sparseMatrix, inpVectors
 
+def structSparse(blockSize= 4, nnz= 2):
+    '''
+    generate traces with 2:4 structured sparsity 
+    (https://images.nvidia.com/aem-dam/en-zz/Solutions/data-center/nvidia-ampere-architecture-whitepaper.pdf)
+    '''
+    sparseMatrix = np.zeros((matrixHeight, matrixWidth))
+    for i in range(matrixHeight):
+        for j in range(matrixWidth//blockSize):
+            nonZeros = random.sample(range(blockSize), nnz)
+            for idx in nonZeros:
+                sparseMatrix[i][blockSize*j + idx] = np.random.randn(1)
+    return sparseMatrix
+
 if __name__ == '__main__':
     file_sparse = "sparseM.txt"
+    file_structSparse = "structSparseM.txt"
     file_vector = "inpV.txt"
     matrixHeight = 64
     matrixWidth = 64
     numInputs = 80
     sparsityRate = 0.4  # approx 40% of the elements will be 0
     #
+    '''
     spM, inpV = irregSparse()
     spM = np.matrix(spM)
     inpV = np.matrix(inpV)
@@ -38,6 +54,12 @@ if __name__ == '__main__':
     csrM = csr_matrix(spM)
     print(csrM.indices)
     print(csrM.indptr)
+    '''
+    struct_spM = structSparse()
+    struct_spM = np.matrix(struct_spM)
+    with open(file_structSparse, 'w') as f2:
+        for line in struct_spM:
+            np.savetxt(f2, line, delimiter= ", ", header= "{", footer= "},", fmt= "%0.4f", comments= '')
     
 
     
